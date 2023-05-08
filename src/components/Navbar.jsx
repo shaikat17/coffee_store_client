8cae33ef4c6051ba9f474995facd70b82f733cc2
shaikat17 <shaikatpal56@gmail.com>
2023-05-08 15:47:42 +0600

    made an small changes on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import logoImg from '../../public/images/more/logo1.png'
 const Navbar = () => {
   return (
     <div className="navbar-center text-center" style={{backgroundImage: `url('${navImg}')`}}>
-      <NavLink className=" flex items-center justify-center font-black text-2xl" to="/"> <img className="w-16 h-20" src={logoImg} alt="Espresso Emporium" /> Espresso Emporium</NavLink>
+      <NavLink className=" flex items-center justify-center font-black text-2xl text-white" to="/"> <img className="w-16 h-20" src={logoImg} alt="Espresso Emporium" /> Espresso Emporium</NavLink>
     </div>
   );
 };
